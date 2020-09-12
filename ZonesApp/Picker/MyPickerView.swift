966a9308cc9a1ba4c2055063f7a0d7fb88fdd76d
Nikita Galkov <tyler_1991@mail.ru>
2020-09-12 22:02:02 +0300

    12_09_2020 last version

--- ZonesApp/Picker/MyPickerView.swift
+++ ZonesApp/Picker/MyPickerView.swift
@@ -8,54 +8,125 @@
 
 import UIKit
 
-class MyPickerView: UIPickerView, UIPickerViewDataSource, UIPickerViewDelegate {
-    
-//var pickerDataFromArray: FirstViewController?
+//class MyPickerView: UIPickerView, UIPickerViewDataSource, UIPickerViewDelegate {
+//
+////var pickerDataFromArray: FirstViewController?
+//
+//
+// public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//
+//        if pickerData != nil {
+//            return pickerData!.count
+//        } else {
+//            return 0
+//        }
+//    }
+//
+//    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        if pickerData != nil {
+//            return pickerData![row]
+//        } else {
+//            return ""
+//        }
+//
+//    }
+//
+//    //Data of the picker
+//
+//public var pickerData: [String]?  {
+//    didSet {
+//        super.delegate = self
+//        super.dataSource = self
+//        self.reloadAllComponents()
+//    }
+//}
+//
+////Get the selected value from picker
+//
+//public var selectedValue: String {
+//    get {
+//        if pickerData != nil {
+//            return pickerData![selectedRow(inComponent: 0)]
+//        } else {
+//            return ""
+//        }
+//    }
+//}
+//
+//
+//}
 
-    
- public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-    
-        if pickerData != nil {
-            return pickerData!.count
-        } else {
-            return 0
-        }
-    }
-
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerData != nil {
-            return pickerData![row]
-        } else {
-            return ""
-        }
-    
-    }
-
-    //Data of the picker
-    
-public var pickerData: [String]?  {
-    didSet {
-        super.delegate = self
-        super.dataSource = self
-        self.reloadAllComponents()
-    }
+// протоколы не до конца тему понял, но продолжу работать, что понять все от А до Я
+protocol PickerViewDelegate: class {
+    func didTapDone()
+    func didTapCancel()
 }
 
-//Get the selected value from picker
+class MyPickerView: UIPickerView {
 
-public var selectedValue: String {
-    get {
-        if pickerData != nil {
-            return pickerData![selectedRow(inComponent: 0)]
-        } else {
-            return ""
-        }
+    public private(set) var toolbar: UIToolbar?
+    public private(set) var picker: UIPickerView?
+    public weak var pickerDelegate: PickerViewDelegate?
+
+    //эти инициализаторы мне ХСОД сам предложил сделать, с ними еще не разобрался
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInitToolbar()
     }
-}
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInitToolbar()
+    }
+
+
+    
+    private func commonInitToolbar() {
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = false
+        toolBar.tintColor = .gray
+        toolBar.backgroundColor = .gray
+        toolBar.barTintColor = .black
+        toolBar.sizeToFit()
+        toolBar.autoresizingMask = .flexibleHeight
+
+        
+
+        let cancelButton = UIBarButtonItem(title: "Clear",
+                                           style: .done,
+                                           target: self,
+                                           action: #selector(cancelTapped(_:)))
+        cancelButton.tintColor = .white
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                        target: nil,
+                                        action: nil)
+
+        let doneButton = UIBarButtonItem(title: "Done",
+                                         style: .done,
+                                         target: self,
+                                         action: #selector(doneTapped(_:)))
+        doneButton.tintColor = .white
+        toolBar.items = [cancelButton, flexSpace, doneButton]
+        toolBar.isUserInteractionEnabled = true
+        
+        self.toolbar = toolBar
+       
+        
+    }
     
     
+
+    @objc func doneTapped(_ button: UIBarButtonItem?) {
+        self.pickerDelegate?.didTapDone()
+    }
+
+    @objc func cancelTapped(_ button: UIBarButtonItem?) {
+        self.pickerDelegate?.didTapCancel()
+    }
 }
--- ZonesApp/ViewController/FirstViewController.swift
+++ ZonesApp/ViewController/FirstViewController.swift
@@ -8,111 +8,174 @@
 
 import UIKit
 
-
-class FirstViewController: UIViewController, UITextFieldDelegate {
-    
-    @IBOutlet weak var newZonesTextField: UITextField!
-    @IBOutlet weak var reuseZonesTextField: UITextField!
-    var picker: MyPickerView?
-    var pickerAccessory: UIToolbar?
-    let reusePickerData: [String] = { (0...14).map { "\($0)" }}()
-    
-
+class FirstViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource, PickerViewDelegate {
+//class FirstViewController: UIViewController, UITextFieldDelegate {
+//
+      @IBOutlet weak var newZonesTextField: UITextField!
+      @IBOutlet weak var reuseZonesTextField: UITextField!
+//    var picker: MyPickerView?
+//    var pickerAccessory: UIToolbar?
+//
+//
+//
+//
+//    let reusePickerData: [String] = { (0...14).map { "\($0)" }}()
+//
+//
+//    private var privateZone = DataManager.default.zoneData
+//
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//               newZonesTextField.resignFirstResponder()
+//               reuseZonesTextField.resignFirstResponder()
+//               return true
+//           }
+//
+//
+////    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+////               super.touchesBegan(touches, with: event)
+////               picker?.endEditing(true)
+////       }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//
+//        reuseZonesTextField.text = String(privateZone.reuseZones)
+//
+//        //Picker
+//        picker = MyPickerView()
+//        picker?.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+//        picker?.backgroundColor = UIColor.white
+//        picker?.pickerData = reusePickerData
+//        reuseZonesTextField.inputView = picker
+//        picker?.selectRow(DataManager.default.zoneData.reuseZones, inComponent: 0, animated: true)
+//
+//
+//
+//        //Toolbar
+//        pickerAccessory = UIToolbar()
+//        pickerAccessory?.autoresizingMask = .flexibleHeight
+//        pickerAccessory?.barStyle = .default
+//        pickerAccessory?.barTintColor = UIColor.gray
+//        pickerAccessory?.backgroundColor = UIColor.gray
+//        pickerAccessory?.isTranslucent = false
+//        var frame = pickerAccessory?.frame
+//        frame?.size.height = 44.0
+//        pickerAccessory?.frame = frame!
+//        reuseZonesTextField.inputAccessoryView = pickerAccessory
+//
+//        addDoneClearButtonTo(reuseZonesTextField)
+//
+//    }
+//
+//}
+//
+//extension FirstViewController {
+//
+//    //Toolbar buttons
+//
+//    private func addDoneClearButtonTo(_ textField: UITextField) {
+//
+//        let cancelButton = UIBarButtonItem(title: "Clear",
+//                                           style: .done,
+//                                           target: self,
+//                                           action: #selector(FirstViewController.cancelBtnClicked(_:)))
+//        cancelButton.tintColor = UIColor.white
+//
+//        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+//                                        target: nil,
+//                                        action: nil)
+//
+//        let doneButton = UIBarButtonItem(title: "Done",
+//                                         style: .done,
+//                                         target: self,
+//                                         action: #selector(FirstViewController.doneBtnClicked(_:)))
+//        doneButton.tintColor = UIColor.white
+//        pickerAccessory?.items = [cancelButton, flexSpace, doneButton]
+//
+//
+//    }
+//
+//
+//
+//// clear button clicked
+//   @objc func cancelBtnClicked(_ button: UIBarButtonItem?) {
+//            reuseZonesTextField?.resignFirstResponder()
+//            reuseZonesTextField.text = "0"
+//            DataManager.default.zoneData.reuseZones = 0
+//
+//        }
+// // done button clicked
+//   @objc func doneBtnClicked(_ button: UIBarButtonItem?) {
+//            reuseZonesTextField?.resignFirstResponder()
+//            reuseZonesTextField.text = picker?.selectedValue
+//    if let newValue = picker?.selectedValue {
+//        DataManager.default.zoneData.reuseZones = Int(newValue)!
+//
+//}
+//
+//}
+//}
     private var privateZone = DataManager.default.zoneData
-    
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-               newZonesTextField.resignFirstResponder()
-               reuseZonesTextField.resignFirstResponder()
-               return true
-           }
-           
-          
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//               super.touchesBegan(touches, with: event)
-//               picker?.endEditing(true)
-//       }
-    
+
+    fileprivate let picker = MyPickerView()
+    fileprivate let reusePickerData: [String] = { (0...14).map { "\($0)" }}()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       
-        reuseZonesTextField.text = String(privateZone.reuseZones)
     
-        //Picker
-        picker = MyPickerView()
-        picker?.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        picker?.backgroundColor = UIColor.white
-        picker?.pickerData = reusePickerData
-        reuseZonesTextField.inputView = picker
-        picker?.selectRow(DataManager.default.zoneData.reuseZones, inComponent: 0, animated: true)
+        reuseZonesTextField.text = String(privateZone.reuseZones)
         
+        self.reuseZonesTextField.inputView = self.picker
+        self.reuseZonesTextField.inputAccessoryView = self.picker.toolbar
+
+        self.picker.dataSource = self
+        self.picker.delegate = self
+        self.picker.pickerDelegate = self
+        self.picker.selectRow(DataManager.default.zoneData.reuseZones, inComponent: 0, animated: true)
         
-        
-        //Toolbar
-        pickerAccessory = UIToolbar()
-        pickerAccessory?.autoresizingMask = .flexibleHeight
-        pickerAccessory?.barStyle = .default
-        pickerAccessory?.barTintColor = UIColor.gray
-        pickerAccessory?.backgroundColor = UIColor.gray
-        pickerAccessory?.isTranslucent = false
-        var frame = pickerAccessory?.frame
-        frame?.size.height = 44.0
-        pickerAccessory?.frame = frame!
-        reuseZonesTextField.inputAccessoryView = pickerAccessory
-        
-        addDoneClearButtonTo(reuseZonesTextField)
-        
+        self.picker.reloadAllComponents()
     }
     
 }
 
 extension FirstViewController {
-    
-    //Toolbar buttons
-    
-    private func addDoneClearButtonTo(_ textField: UITextField) {
 
-        let cancelButton = UIBarButtonItem(title: "Clear",
-                                           style: .done,
-                                           target: self,
-                                           action: #selector(FirstViewController.cancelBtnClicked(_:)))
-        cancelButton.tintColor = UIColor.white
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.reusePickerData.count
+    }
 
-        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
-                                        target: nil,
-                                        action: nil)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
-        let doneButton = UIBarButtonItem(title: "Done",
-                                         style: .done,
-                                         target: self,
-                                         action: #selector(FirstViewController.doneBtnClicked(_:)))
-        doneButton.tintColor = UIColor.white
-        pickerAccessory?.items = [cancelButton, flexSpace, doneButton]
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.reusePickerData[row]
+    }
 
-    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.reuseZonesTextField.text = self.reusePickerData[row]
+        DataManager.default.zoneData.reuseZones = Int(reuseZonesTextField.text!)!
     }
     
-    
-    
-// clear button clicked
-   @objc func cancelBtnClicked(_ button: UIBarButtonItem?) {
-            reuseZonesTextField?.resignFirstResponder()
-            reuseZonesTextField.text = "0"
-            DataManager.default.zoneData.reuseZones = 0
-    
-        }
- // done button clicked
-   @objc func doneBtnClicked(_ button: UIBarButtonItem?) {
-            reuseZonesTextField?.resignFirstResponder()
-            reuseZonesTextField.text = picker?.selectedValue
-    if let newValue = picker?.selectedValue {
-        DataManager.default.zoneData.reuseZones = Int(newValue)!
-            
-}
 
 }
+
+extension FirstViewController {
+
+    func didTapDone() {
+//        let row = self.picker.selectedRow(inComponent: 0)
+//        self.picker.selectRow(row, inComponent: 0, animated: false)
+//        self.reuseZonesTextField.text = self.reusePickerData[row]
+//        DataManager.default.zoneData.reuseZones = Int(self.reusePickerData[row])!
+        self.reuseZonesTextField.resignFirstResponder()
+    }
+
+    func didTapCancel() {
+        self.reuseZonesTextField.text = "0"
+        DataManager.default.zoneData.reuseZones = 0
+        self.reuseZonesTextField.resignFirstResponder()
+    }
 }
-
-
 
