19837ac10b2807806f886db35f0f1112b118bd5f
Nikita Galkov <tyler_1991@mail.ru>
2020-09-25 21:56:57 +0300

    reorder zone number after delete

--- ZonesApp/Model and DataManager/ZoneModel.swift
+++ ZonesApp/Model and DataManager/ZoneModel.swift
@@ -22,15 +22,16 @@ public struct ZoneData: Codable {
 
 struct NewZones: Codable {
     
-    var zoneNumber: Int = 1
-    var zoneName: String
-    var outletNumber: Int
+    var zoneNumber: Int?
+    var zoneName: String?
+    var outletNumber: Int?
     
-    init (zoneNumber: Int, zoneName: String, outletNumber: Int) {
-        self.zoneNumber = zoneNumber
-        self.zoneName = zoneName
-        self.outletNumber = outletNumber
-    }
+//    init(zoneNumber: Int, zoneName: String, outletNumber: Int) {
+//        self.zoneNumber = zoneNumber
+//        self.zoneName = zoneName
+//        self.outletNumber = outletNumber
+//    }
+    
     
 }
 
--- ZonesApp/ViewController/SecondViewController.swift
+++ ZonesApp/ViewController/SecondViewController.swift
@@ -8,27 +8,26 @@
 
 import UIKit
 
-
-
-
 class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    
+    
 @IBOutlet public var tableView: UITableView!
-    
-    
-    
-    private var privateNewZone = DataManager.default.zoneData.newZones
+    var zoneArray = [NewZones]()
+
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
     
     override func viewDidLoad() {
     super.viewDidLoad()
-        
-
+    
         
         let nib = UINib(nibName: "DemoTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "DemoTableViewCell")
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.reloadData()
+        //tableView.reloadData()
         tableView.rowHeight = 91.0
         
     
@@ -37,7 +36,6 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     
     @IBAction func doneButtonSaveReturn(_ sender: Any) {
-        
         self.dismiss(animated: true, completion: nil)
     }
     
@@ -47,18 +45,18 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return 1
-       }
+            return zoneArray.count
+    }
+
        
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DemoTableViewCell", for: indexPath) as! DemoTableViewCell
-        
-        let zone = DataManager.default.zoneData.newZones[indexPath.row]
-        cell.zoneLabel.text = "Zone " + String(zone.zoneNumber)
-        cell.zoneTextField.text = zone.zoneName
-        cell.outletTextField.text = String(zone.outletNumber)
-        self.tableView.reloadData()
-            return cell
+        var zone = zoneArray[indexPath.row]
+        zone.zoneNumber = indexPath.row + 1
+        cell.zoneLabel.text = "Zone " + String(zone.zoneNumber!)
+       
+
+        return cell
         
        }
     
@@ -66,35 +64,40 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return 1
     }
     
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-           return true
-       }
+//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//           return true
+//       }
        
 //       delete row by swipe
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
            if editingStyle == .delete {
-            tableView.beginUpdates()
-            tableView.deleteRows(at: [indexPath as IndexPath], with: .automatic)
-            tableView.endUpdates()
+            zoneArray.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath as IndexPath], with: .automatic)
+            self.tableView.reloadData()
+            //self.perform(#selector(reloadTable), with: nil, afterDelay: 15)
            }
        }
     
+   
     
-
+//    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+//        let rowToMove = zoneArray[sourceIndexPath.row]
+//        zoneArray.remove(at: sourceIndexPath.row)
+//        zoneArray.insert(rowToMove, at: destinationIndexPath.row)
+//    }
+    
     //insert new row button
     @IBAction func addButtonTapped(_ sender: UIButton) {
         
-        DataManager.default.zoneData.newZones.append(NewZones(zoneNumber: DataManager.default.zoneData.newZones.count + 1, zoneName: "", outletNumber: 0))
-        let indexPath = IndexPath(row: DataManager.default.zoneData.newZones.count - 1, section: 0)
-        
+        let indexPath = IndexPath(row: zoneArray.count, section: 0)
         self.tableView.beginUpdates()
-        
         self.tableView.insertRows(at: [indexPath], with: .automatic)
+        zoneArray.append(NewZones(zoneNumber: indexPath.row + 1, zoneName: nil, outletNumber: nil))
         self.tableView.endUpdates()
-        }
-    }
+        
+  }
 
-
+}
 
 
 
